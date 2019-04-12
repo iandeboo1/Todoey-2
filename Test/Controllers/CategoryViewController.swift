f39ddecb6198d79c90a00510e4defcd8f11a95af
Ian DeBoo <ianjdeboo@icloud.com>
2019-04-11 18:00:41 -0600

    Persisting data using CoreData complete

---
+++ Test/Controllers/CategoryViewController.swift
@@ -0,0 +1,120 @@
+//
+//  CategoryViewController.swift
+//  Test
+//
+//  Created by Ian DeBoo on 4/11/19.
+//  Copyright © 2019 Ian DeBoo. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    var categoryArray = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadCategories()
+    }
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        let category = categoryArray[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        return cell
+    }
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+        
+            destinationVC.selectedCategory = categoryArray[indexPath.row]
+        }
+        
+    }
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    //MARK: - Data Manipulation Methods
+    
+    func saveCategories() {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        do {
+            categoryArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    
+    
+    
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    //MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) {
+            (action) in
+            
+            let newCateogry = Category(context: self.context)
+            newCateogry.name = textField.text!
+            
+            self.categoryArray.append(newCateogry)
+            
+            self.saveCategories()
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField {
+            (alertTextField) in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
--- Test/Controllers/TodoListViewController.swift
+++ Test/Controllers/TodoListViewController.swift
@@ -7,18 +7,26 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-    //this determines a path to save user data in a file called "Items.plist"
+    var selectedCategory : Category? {
+        didSet {
+            //this keyword allows things to happen once an optional value is set
+            loadItems()
+        }
+    }
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    //this line taps into a Singleton for the AppDelegate class and allows the context (a.k.a. the temporary holding area) to be accessed from this class
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath)
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         loadItems()
     }
@@ -37,7 +45,7 @@ class TodoListViewController: UITableViewController {
         
         let item = itemArray[indexPath.row]
         
-        cell.textLabel?.text = item.label
+        cell.textLabel?.text = item.title
         
         cell.accessoryType = item.done ? .checkmark : .none
         //this is called a Ternary Operator and basically takes the places of and shortens an if-else statement in certain cases
@@ -50,6 +58,10 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
+        
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        //these two lines together remove an item from the array and the core database, respectively; THEY ALSO HAVE TO BE IN THIS ORDER
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
@@ -69,8 +81,11 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) {
             (action) in
             
-            let newItem = Item()
-            newItem.label = textField.text!
+            let newItem = Item(context: self.context)
+            //this creats a new item and places it within the context
+            newItem.title = textField.text!
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             
             self.itemArray.append(newItem)
             
@@ -92,30 +107,74 @@ class TodoListViewController: UITableViewController {
     //MARK: - Model Manipulation Actions
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try self.context.save()
         } catch {
-            print("Error in encoding itemArray, \(error)")
+            print("Error saving context, \(error)")
         }
         
         tableView.reloadData()
     }
     
-    func loadItems() {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        //the with is an external input so it's what the input is called when used elsewhere, while request is used here in the code block
         
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error decoding itemArray, \(error)")
-            }
+        let categoryPredicate = NSPredicate(format: "parentCategory.name CONTAINS %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
         }
-
+        
+        do {
+           itemArray = try context.fetch(request)
+            //this actaully pulls data to the context area for reading
+        } catch {
+            print("Error fetching data from context, \(error)")
+        }
+        
+        self.tableView.reloadData()
     }
     
 }
+//////////////////////////////////////////////////////////////////////////////////
+//MARK: - Search Bar Methods
 
+extension TodoListViewController: UISearchBarDelegate {
+    //this allows for more functionalities for a single class and also adds a further level of code organization by butting only code related to this set of rules (search bar) in this extension section
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        //this assigns fetching funcitonality to the request variable
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        //this line creates a query; the format is in a language called NS predicate and there's an entire bookmarked cheat sheet if need be; the [cd] isn't necessary but removes case sensitivity
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        //this creates a sort filter that sorts the titles in ascending alphabetical order; it also adds the filter to the fetch request; notice that it is formatted as an array becuase multiple different filters can all be added
+        
+        loadItems(with: request, predicate: predicate)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.count > 0 {
+            let request: NSFetchRequest<Item> = Item.fetchRequest()
+            
+            request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+            
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+            
+            loadItems(with: request)
+            
+        } else {
+            loadItems()
+    
+            DispatchQueue.main.async {
+                //this runs this function in the fireground; all UI updates should be run this way
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+    
+}
